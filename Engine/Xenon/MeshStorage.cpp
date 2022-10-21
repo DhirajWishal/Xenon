cd6c2d1c265ffa6e9aabea70ab9b255526164d14
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-10-22 02:17:25 +0530

    Fixed some compile-time errors.

--- Engine/Xenon/MeshStorage.cpp
+++ Engine/Xenon/MeshStorage.cpp
@@ -3,44 +3,8 @@
 
 #include "MeshStorage.hpp"
 
-#include <rapidjson/document.h>
+#include <tiny_gltf.h>
 #include <fstream>
-
-namespace /* anonymous */
-{
-	/**
-	 * Load the RapidJSON document from the file.
-	 *
-	 * @param file The file to load the JSON data from.
-	 * @return The JSON document.
-	 */
-	rapidjson::Document LoadDocument(const std::filesystem::path& file)
-	{
-		// Open the file in the read mode.
-		auto glTF = std::fstream(file, std::ios::in | std::ios::binary | std::ios::ate);
-
-		// Load the JSON data if possible.
-		rapidjson::Document document;
-		if (glTF.is_open())
-		{
-			// Get the file size.
-			const auto size = glTF.tellg();
-			glTF.seekg(0);
-
-			// Create the string and load the data to it.
-			std::string fileData;
-			fileData.resize(size);
-
-			glTF.read(fileData.data(), size);
-
-			// Parse the loaded data and close the file.
-			document.Parse(fileData.c_str());
-			glTF.close();
-		}
-
-		return document;
-	}
-}
 
 namespace Xenon
 {
@@ -53,7 +17,15 @@ namespace Xenon
 			return MeshStorage(instance);
 
 		// Load the model data.
-		const auto modelData = LoadDocument(file);
+		tinygltf::Model model;
+		std::string errorString;
+		std::string warningString;
+
+		tinygltf::TinyGLTF loader;
+		if (loader.LoadASCIIFromFile(&model, &errorString, &warningString, file.string()))
+		{
+
+		}
 
 		return MeshStorage(instance);
 	}
