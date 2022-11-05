176182288537bb3abe0e06ad700f78a392e205b1
Dhiraj Wishal <wishaldhiraj@gmail.com>
2022-11-06 01:07:13 +0530

    Added shell scripts and custom targets to automate CMake configuration.
    * Added shell files for both bash and PowerShell to automate the CMake file generation.
    * Added a custom target for MSVC so that we can build all the necessary CMake files from within the IDE.

---
+++ Bootstrap.sh
@@ -0,0 +1,11 @@
+# Copyright 2022 Dhiraj Wishal 
+# SPDX-License-Identifier: Apache-2.0
+# 
+# Xenon bootstrap file
+
+# Create the Build directory if it doesn't exist.
+mkdir -p Build
+
+# Change directory to that directory and setup the CMake files.
+cd Build
+cmake ..
