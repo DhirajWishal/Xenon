c86af9652dcdf272b9071245e64ad0e23a6715fb
Dhiraj Wishal <wishaldhiraj@gmail.com>
2023-03-02 15:42:56 +0530

    Implemented the Xenon asset packager.
    * The Xenon asset packager command line tool can now take in an input json document with all the asset information and it can output a CBOR format package.

---
+++ Engine/XenonAssetPackager/Packager.cpp
@@ -0,0 +1,98 @@
+// Copyright 2022-2023 Dhiraj Wishal
+// SPDX-License-Identifier: Apache-2.0
+
+#include "Packager.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <iostream>
+#include <fstream>
+
+using JsonDocument = nlohmann::json;
+
+namespace Xenon
+{
+	Packager::Packager(const std::filesystem::path& inputFile, const std::filesystem::path& outputFile)
+		: m_InputFile(inputFile)
+		, m_OutputFile(outputFile)
+	{
+	}
+
+	uint32_t Packager::package() const
+	{
+		// Check if the input file exists.
+		if (!std::filesystem::exists(m_InputFile))
+		{
+			std::cout << "The input file does not exist!" << std::endl;
+			return -1;
+		}
+
+		// Load the input file.
+		std::ifstream inputFile(m_InputFile);
+		JsonDocument inputData = JsonDocument::parse(inputFile);
+		inputFile.close();
+
+		// Load all the data to a new json document.
+		JsonDocument loadedData;
+		for (auto itr = inputData.begin(); itr != inputData.end(); ++itr)
+		{
+			const auto& jsonData = *itr;
+			const auto& key = itr.key();
+
+			// Check if we need to load anything.
+			if (jsonData.is_object() && jsonData.contains("file") && jsonData.contains("type"))
+			{
+				auto& object = loadedData[key];
+				object["type"] = jsonData["type"];
+				object["bytes"] = loadFileData(std::string(jsonData["file"]));
+			}
+			else
+			{
+				loadedData[key] = jsonData;
+			}
+		}
+
+		// Compress and write everything to the output file.
+		const auto output = JsonDocument::to_cbor(loadedData);
+		std::ofstream outputFile(m_OutputFile, std::ios::out | std::ios::binary);
+		if (outputFile.is_open())
+		{
+			outputFile.write(std::bit_cast<const char*>(output.data()), output.size());
+		}
+		else
+		{
+			std::cout << "Failed to create and write the output file!" << std::endl;
+
+			outputFile.close();
+			return -2;
+		}
+
+		outputFile.close();
+
+		// Inform the user that the operation was successful and exit.
+		std::cout << "Successfully created the Xenon package file: " << m_OutputFile << std::endl;
+		return 0;
+	}
+
+	std::vector<std::byte> Packager::loadFileData(const std::filesystem::path& file) const
+	{
+		std::vector<std::byte> bytes;
+
+		std::ifstream inputFile(file, std::ios::in | std::ios::binary | std::ios::ate);
+		if (inputFile.is_open())
+		{
+			const auto size = inputFile.tellg();
+			inputFile.seekg(0);
+
+			bytes.resize(size);
+			inputFile.read(std::bit_cast<char*>(bytes.data()), size);
+		}
+		else
+		{
+			std::cout << "Failed to load file: " << file << std::endl;
+		}
+
+		inputFile.close();
+		return bytes;
+	}
+}
---
+++ Engine/XenonAssetPackager/Packager.hpp
@@ -0,0 +1,74 @@
+// Copyright 2022-2023 Dhiraj Wishal
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <filesystem>
+#include <vector>
+#include <bit>
+
+namespace Xenon
+{
+	/**
+	 * Packager class.
+	 * This class reads all the information from the input JSON document and packs them all into CBOR format and saves them to the
+	 * output file.
+	 *
+	 * The input data format:
+	 * {
+	 *		"entry1": {
+	 *			"file": "something.txt",
+	 *			"type": "bytes"
+	 *		},
+	 *		"entry2": 100,
+	 *		"entry3": {
+	 *			"x": "something",
+	 *			"y": 200
+	 *		}
+	 * }
+	 *
+	 * The output format:
+	 * {
+	 *		"entry1": {
+	 *			"bytes": [...],
+	 *			"type": "bytes"
+	 *		},
+	 *		"entry2": 100,
+	 *		"entry3": {
+	 *			"x": "something",
+	 *			"y": 200
+	 *		}
+	 * }
+	 */
+	class Packager final
+	{
+	public:
+		/**
+		 * Explicit constructor.
+		 *
+		 * @param inputFile The input file.
+		 * @param outputFile The output file.
+		 */
+		explicit Packager(const std::filesystem::path& inputFile, const std::filesystem::path& outputFile);
+
+		/**
+		 * Package everything to CBOR.
+		 *
+		 * @return The return status.
+		 */
+		[[nodiscard]] uint32_t package() const;
+
+	private:
+		/**
+		 * Load the file data.
+		 *
+		 * @param file The file to load the data from.
+		 * @return The loaded bytes.
+		 */
+		[[nodiscard]] std::vector<std::byte> loadFileData(const std::filesystem::path& file) const;
+
+	private:
+		std::filesystem::path m_InputFile;
+		std::filesystem::path m_OutputFile;
+	};
+}
